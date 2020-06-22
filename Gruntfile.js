ce6346a30008646e9eff2c4a65745858c198f6a1
traore <tresonance2@gmail.com>
2020-06-23 01:57:03 +0200

    angularjs-tuto7

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+
+    // Load Grunt tasks declared in the package.json file
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Configure Grunt 
+    grunt.initConfig({
+
+        // grunt-contrib-connect will serve the files of the project
+        // on specified port and hostname
+        connect: {
+            all: {
+                options: {
+                    port: 9000,
+                    hostname: "0.0.0.0",
+                    // Prevents Grunt to close just after the task (starting the server) completes
+                    // This will be removed later as `watch` will take care of that
+                    keepalive: true
+                }
+            }
+        }
+    });
+
+    // Creates the `server` task
+    grunt.registerTask('server', [
+        'connect'
+    ]);
+};
